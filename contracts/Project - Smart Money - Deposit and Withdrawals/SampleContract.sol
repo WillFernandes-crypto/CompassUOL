3990f7b879fc1049ac69e07507b03312466834e4
WillFernandes-crypto <wilsonfernandes390@gmail.com>
2024-12-14 14:45:04 -0300

    Updating Files

---
+++ contracts/Project - Smart Money - Deposit and Withdrawals/SampleContract.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.28;
+
+contract SampleContract {
+
+    string public myString = "Hello World";
+
+    function updateString(string memory _newString) public {
+        myString = _newString;
+    }
+
+}
